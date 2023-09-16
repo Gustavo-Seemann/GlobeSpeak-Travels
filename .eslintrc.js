4a400b111003066432fe8723100a0c5a9b3b5aa9
Luiz Gustavo S <seemann24@hotmail.com>
2023-09-15 22:07:43 -0300

    chore: setup config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/vue3-strongly-recommended", "eslint:recommended"],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: "@babel/eslint-parser",
   },
+  plugins: ["vue"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "vue/no-multi-spaces": 0,
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faChevronRight, faChevronLeft, faArrowRight, faEllipsis, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+/* add icons to the library */
+library.add(faChevronRight, faChevronLeft, faArrowRight, faFacebook, faInstagram, faWhatsapp, faLinkedin, faEllipsis, faBars);
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount('#app');
